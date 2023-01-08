51fce77aea8b57afc82c992041b80ad9de833838
AuriRex <xaurirexx@gmail.com>
2023-01-09 00:08:48 +0100

    Initial commit v2

---
+++ DoorSoundCheeseFix/DSCFMod.cs
@@ -0,0 +1,47 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using System.Collections;
+using System.Reflection;
+using UnityEngine;
+
+namespace DoorSoundCheeseFix
+{
+    [BepInPlugin(GUID, nameof(DoorSoundCheeseFix), VERSION)]
+    public class DSCFMod : BasePlugin
+    {
+        public const string GUID = "dev.aurirex.gtfo.doorsoundcheesefix";
+        public const string VERSION = "1.0.0";
+
+        private HarmonyLib.Harmony _harmonyInstance;
+        private static MonoBehaviour _mainComponent;
+
+        public override void Load()
+        {
+            _mainComponent = AddComponent<CoroutineComponentThing>();
+
+            _harmonyInstance = new HarmonyLib.Harmony(GUID);
+            _harmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        public static Coroutine StartCoroutine(IEnumerator routine)
+        {
+            return BepInEx.Unity.IL2CPP.Utils.MonoBehaviourExtensions.StartCoroutine(_mainComponent, routine);
+        }
+
+        public static void StopCoroutine(Coroutine coroutine)
+        {
+            _mainComponent.StopCoroutine(coroutine);
+        }
+
+        public class CoroutineComponentThing : MonoBehaviour
+        {
+            public CoroutineComponentThing(IntPtr ptr) : base(ptr) { }
+
+            public void Awake()
+            {
+                DontDestroyOnLoad(this);
+                hideFlags = HideFlags.HideAndDontSave;
+            }
+        }
+    }
+}
---
+++ Package/README.md
@@ -0,0 +1,8 @@
+# Overview
+
+Reworks how the small (weak) doors opening and closing behavior works:
+
+* Once the door has opened one fourth of the way, the door is considered open and sound as well as enemies can now go through.
+* Once the door has closed three fourths of the way, the door is considered closed and no sound or enemies can go through anymore.
+
+This does **not** fix "door bug", aka the bug that makes enemies unable to walk through a broken door.
